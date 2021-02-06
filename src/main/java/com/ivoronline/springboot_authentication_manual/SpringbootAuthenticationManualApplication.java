8870d98522f84afe42f538428495661c2405d8dd
igoreta <ivoronline@gmail.com>
2021-02-06 13:08:09 +0100

    Initial commit

---
+++ src/main/java/com/ivoronline/springboot_authentication_manual/SpringbootAuthenticationManualApplication.java
@@ -0,0 +1,13 @@
+package com.ivoronline.springboot_authentication_manual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAuthenticationManualApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbootAuthenticationManualApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/ivoronline/springboot_authentication_manual/SpringbootAuthenticationManualApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivoronline.springboot_authentication_manual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAuthenticationManualApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
